a71824dff81ce1f1418413e164b6eabd0b244a5d
pbitts <freezow@DESKTOP-PS3SB60.localdomain>
2025-09-23 20:31:10 -0300

    add router for graphql

--- src/infra/api/http/main.py
+++ src/infra/api/http/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
+from src.infra.api.graphql.schema_pydantic import graphql_app as graphql_router
 from src.infra.api.http.cast_member_router import router as cast_member_router
 from src.infra.api.http.category_router import router as category_router
 from src.infra.api.http.genre_router import router as genre_router
+from src.infra.api.http.video_router import router as video_router
 
 app = FastAPI()
 app.include_router(category_router, prefix="/categories")
 app.include_router(cast_member_router, prefix="/cast_members")
 app.include_router(genre_router, prefix="/genres")
+app.include_router(video_router, prefix="/videos")
+
+app.include_router(graphql_router, prefix="/graphql")
 
 
 @app.get("/healthcheck/")
